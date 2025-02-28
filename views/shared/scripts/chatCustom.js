aa638534043d19cc489419fde0f428d481cf9988
ivan <ivanesconf@gmail.com>
2025-02-28 22:54:18 +0100

    Actualización de las APIs e integración en el chat

--- views/shared/scripts/chatCustom.js
+++ views/shared/scripts/chatCustom.js
@@ -1,9 +1,13 @@
 class chatGlobal extends HTMLElement{
     constructor() {
         super();
-        
+
         this.setAttribute("id", "chatGlobal-customElement");
-        this.shadow = this.attachShadow({mode: "open"});
+        this.shadow = this.attachShadow({ mode: "open" });
+
+        this.link = document.createElement("link");
+        this.link.setAttribute("rel", "stylesheet");
+        this.link.setAttribute("href", "./styles/chatCustom.css");
 
         this.base = document.createElement("section");
         let chat = document.createElement("div");
@@ -11,14 +15,15 @@ class chatGlobal extends HTMLElement{
         let input = document.createElement("input");
         let boton = document.createElement("button");
 
-        boton.addEventListener("click", () => {
-            if(input.value){
-                this.enviarMensaje(input.value);
+        boton.addEventListener("click", async () => {
+            //requestDnDInfo(input.value);
+            if (input.value) {
+                this.enviarMensaje(await this.evaluarMensaje(input.value));
             }
         })
 
-        input.addEventListener("keypress", (e) => {          
-            if(e.key == "Enter"){
+        input.addEventListener("keypress", (e) => {
+            if (e.key == "Enter") {
                 boton.dispatchEvent(new Event("click"));
             }
         })
@@ -36,7 +41,6 @@ class chatGlobal extends HTMLElement{
     }
 
     connectedCallback() {
-        //super.connectedCallback();
         this.shadow.innerHTML = `
         <style>
             section {
@@ -67,10 +71,66 @@ class chatGlobal extends HTMLElement{
                 }
             }
         </style>
-        `
+        `;
+
         this.shadow.appendChild(this.base);
+        this.shadow.appendChild(this.link);
     }
 
+    //FUNCIONES DEL CHAT
+
+    //Evaluar un mensaje para establecer el formato
+    async evaluarMensaje(mnsRaw) {
+        if (!mnsRaw) return;
+        let mns = mnsRaw.trim();
+        if (mns.startsWith("/")) {
+            let parts = mns.trim().substring(1).split(" ");
+            let cmd = parts[0].toLowerCase() || "";
+            let data = parts.slice(1).join(" ") || "";
+            switch (cmd) {
+                case "raza": {
+                    mns = await buscar("races", data)
+                        .then(async (response) => {
+                            let name = await traducir(response.name);
+                            let desc = await traducir(response.size_description);
+                            return `<p class="info">
+                                        <a id="responseTitle">${name}</a>:
+                                        <br>
+                                        <a id="responseDatum">${desc}</a>
+                                    </p>`;
+                        })
+                        .catch(() => {
+                            return "<p class='info'>No se encontró la clase.</p>";
+                        });
+                    break;
+                }
+                case "rasgo": {
+                    mns = await buscar("traits", data)
+                        .then(async (response) => {
+                            let name = await traducir(response.name);
+                            let desc = await traducir(response.desc);
+                            return `<p class="info">
+                                        <a id="responseTitle">${name}</a>:
+                                        <br>
+                                        <a id="responseDatum">${desc}</a>
+                                    </p>`;
+                        })
+                        .catch(() => {
+                            return "<p class='info'>No se encontró el rasgo.</p>";
+                        });
+                    break;
+                }
+                default: {
+                    mns = "<p class='info'>Comando no reconocido.</p>";
+                }
+            }
+        } else {
+            mns = "<p class='mns'>" + mns + "</p>";
+        }
+        return mns;
+    }
+
+    //Enviar un mensaje "mns", no puede estar vacío o ser un conjunto de espacios
     enviarMensaje(mns) {
         if(mns){
             let chat = document.getElementById("chatGlobal-customElement").shadow.querySelector("#chat-cge");
@@ -82,4 +142,54 @@ class chatGlobal extends HTMLElement{
     }
 }
 
-customElements.define("chat-global", chatGlobal)
+//Cargar el chat
+addEventListener("DOMContentLoaded", () => {
+    if(document.body){
+        console.log("Chat Cargado");
+        document.body.appendChild(document.createElement("chat-global"));
+        customElements.define("chat-global", chatGlobal);
+    } else {
+        console.error("No se ha podido inicializar el chat");
+    }
+})
+
+//FUNCIONES DE APIS
+
+//Buscar información de DnD => ES UNA API GRATUITA QUE NO REQUIERE DE API-KEY
+async function buscar(url, query) {
+    const myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+    return await fetch(`https://www.dnd5eapi.co/api/${url}/${query}`, requestOptions)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("No se encontró el recurso.");
+            }
+            return response.json();
+        })
+        .catch(() => {
+            return;
+        });
+}
+
+//Traducir un mensaje (por defecto, al español) => SE DEBE USAR EL OAUTH EN UN FUTURO
+async function traducir(mns, lang = "es", sourceLang = "en") {
+    const baseUrl = "https://lingva.ml/api/v1";
+    const url = `${baseUrl}/${sourceLang}/${lang}/${encodeURIComponent(mns)}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+        const data = await response.json();
+        return data.translation;
+    } catch (error) {
+        console.error("Error al traducir:", error);
+        return null;
+    }
+}
