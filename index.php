329f032333d01b7543c3f83abb46081b88bff34c
ivan <ivanesconf@gmail.com>
2025-03-16 21:28:35 +0100

    feature-index_v0.1

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
 <?php
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-  //controladores de entrada a las vistas
-  '/' => "controllers/index.controller.php"
-];
-
-session_start();
-if (!isset($_SESSION['userName'])) {
-  session_destroy();
-}
-
-if (array_key_exists($uri, $routes)) {
-  require $routes[$uri];
-} else {
-  header("Location: /");
-}
+ require "./controllers/index.controller.php";
