6e3b613157806864816ddb6f04354622e7c6d4c3
ivan <ivanesconf@gmail.com>
2025-04-18 23:25:01 +0200

    feature-login_register v0.1

---
+++ views/shared/head.php
@@ -0,0 +1,19 @@
+<?php
+    //Iniciamos la sesión (siempre la mentenemos en cada página)
+    session_start();
+
+    //Si el usuario tiene alguna alerta pendiente, la mostramos
+    if(isset($_SESSION["alert"])){
+        echo "<script>alert('".$_SESSION["alert"]."');</script>";
+        //Una vez mostrada la alerta, la eliminamos de la sesión
+        unset($_SESSION["alert"]);
+    }
+?>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>
+    <?php
+        echo $title ?? "ERROR_001";
+    ?>
+</title>
